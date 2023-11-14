738b286af6c6be73c1b8821e428add5eb8c04a01
Varada <mvarada2000@gmail.com>
2023-11-14 16:36:17 +0530

    Nits and Styles Added

--- pages/service-provider/signup.jsx
+++ pages/service-provider/signup.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import { Form, TextInput, Button } from "carbon-components-react";
-
+import { useDispatch, useSelector } from "react-redux";
 import {
   registerWithEmail,
   signinWithGoogle,
 } from "../../store/slices/authSlice";
-import { useDispatch, useSelector } from "react-redux";
-import styles from "../../styles/login.module.scss";
 import { FcGoogle } from "react-icons/fc";
+import styles from "../../styles/login.module.scss";
+import YourImage from '../../public/loginfsp.svg';
+
 
 const SignUp = () => {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { error } = useSelector((state) => state.userAuth);
 
+  const { error } = useSelector((state) => state.userAuth);
   const router = useRouter();
   const dispatch = useDispatch();
 
@@ -32,18 +34,14 @@ const SignUp = () => {
     );
     router.push("/service-provider");
   };
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <Form onSubmit={onSubmit} style={{ width: "300px" }}>
+    <div className={styles.loginPageContainer}>
+      {/* Move the login form to the left */}
+      <div className={styles.loginForm}>
+      <div className={styles.loginHeading}>
+        <h4>Sign Up</h4>
+      </div>
+      <Form onSubmit={onSubmit} style={{ width: "320px" }}>
         <TextInput
           id="userName"
           labelText="Name"
@@ -64,28 +62,33 @@ const SignUp = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button type="submit" style={{ marginTop: "20px", width: "100%" }}>
-          Sign Up as Service Provider
+        <Button type="submit" style={{ marginTop: "20px", paddingLeft: "19%" ,width:"100%", backgroundColor:"#450d73", justifyContent:"space-around",}}>
+        SIGN UP
         </Button>
-      </Form>
-      <div className="mt-5">
-        <Button onClick={handleGoogleSignIn} className={styles.googleSignIn}>
-          <FcGoogle style={{ marginRight: "1rem", height: "20px" }} />
-          Sign in with Google
-        </Button>
-      </div>
+      </Form> <br />
+      <p style={{alignContent: "center"}}>OR</p>
+      <Button onClick={handleGoogleSignIn} className={styles.googleSignIn} style={{backgroundColor: "#450d73",}}>
+        <FcGoogle style={{ marginRight: "1rem", height: "20px" }} />
+        Sign in with Google
+      </Button>
+      <div className="mt-5"></div>
       {error && <p style={{ color: "red" }}>{error}</p>}
-      <div style={{ marginTop: "20px" }}>
-        Already have an account?{" "}
+      <div style={{ marginTop: "20px", textAlign: "center", }}>
+        Already have an account?{" "} <br />
         <a
           onClick={() => router.push("/service-provider/login")}
-          style={{ cursor: "pointer", color: "#0f62fe" }}
+          style={{ cursor: "pointer", color: "#450d73" }}
         >
           Log in
         </a>
       </div>
     </div>
+     {/* Add an image on the right */}
+     <div className={styles.imageContainer}>
+        <Image src={YourImage} alt="Your Image"  className={styles.image} />
+      </div>
+    </div>
   );
-};
+}
 
-export default SignUp;
+ export default SignUp;
